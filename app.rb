4eacaca327213f48ab5157315be1ddf546fdb2f7
Garrett Thompson <gtcruz.gt@gmail.com>
2020-06-11 00:27:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,28 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
     # code other routes/actions here
+    ### To show all the pirates ###
+    get '/pirates' do
+      @pirates = Pirate.all
 
+      erb :'pirates/index'
+    end
+
+    ### route needs to be define to get to the new page ###
+    get '/pirates/new' do
+      erb :'pirates/new'
+    end
+
+    ### To show one pirate ###
+    get '/pirates/:id' do
+      @pirate = Pirate.find(params["id"])
+
+      erb :'pirates/show'
+    end
+
+    post '/pirates' do
+      #binding.pry
+    end
   end
 end
